978f0934506013f67054970b82a68852a9143437
Keneth Sanchez <jairsan2618@gmail.com>
2022-07-09 19:56:05 -0500

    actualizaciones en la pantalla de retroalimentacion

--- app/src/main/java/com/example/jugabilidad2/jugabilidad2_retroalimentacion.java
+++ app/src/main/java/com/example/jugabilidad2/jugabilidad2_retroalimentacion.java
@@ -2,19 +2,21 @@ package com.example.jugabilidad2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Layout;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class jugabilidad2_retroalimentacion extends AppCompatActivity {
 
-    Button jugabilidad2_retro_correcta_btn, jugabilidad2_retro_incorrecta_btn;
-    View retro_correcta, retro_incorrecta;
-    TextView r_correcta, r_incorrecta, r_felicidades_correcta, r_felicidades_incorrecta;
-    ImageButton r_sonido_correcto, r_sonido_incorrecto;
+    Button jugabilidad2_retro_correcta_btn;
+    TextView r_felicidades_correcta;
+    ImageButton r_sonido_correcto;
+    ImageView imgcheck, imglogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,37 +24,32 @@ public class jugabilidad2_retroalimentacion extends AppCompatActivity {
         setContentView(R.layout.activity_jugabilidad2_retroalimentacion);
 
         InicializarControles();
-        cambiarpantalla();
+        Validar();
     }
 
     private void InicializarControles() {
-        jugabilidad2_retro_correcta_btn =  (Button) findViewById(R.id.jugabilidad2_retro_correcta_btn);
-        jugabilidad2_retro_incorrecta_btn = (Button) findViewById(R.id.jugabilidad2_retro_incorrecta_btn);
-        retro_correcta = (View) findViewById(R.id.jugabilidad2_retro_correcta);
-        retro_incorrecta = (View)findViewById(R.id.jugabilidad2_retro_incorrecta);
-        r_correcta = (TextView) findViewById(R.id.txtRespuesta_correcta);
-        r_incorrecta = (TextView) findViewById(R.id.txtRespuesta_incorrecta);
+        jugabilidad2_retro_correcta_btn = (Button) findViewById(R.id.jugabilidad2_retro_correcta_btn);
         r_felicidades_correcta = (TextView) findViewById(R.id.txtFelicidades_correcta);
-        r_felicidades_incorrecta = (TextView) findViewById(R.id.txtFelicidades_incorrecta);
         r_sonido_correcto = (ImageButton) findViewById(R.id.imgSonido_correcto);
-        r_sonido_incorrecto = (ImageButton) findViewById(R.id.imgSonido_incorrecto);
+
+        imgcheck = (ImageView) findViewById(R.id.imgcheck);
+        imglogo = (ImageView) findViewById(R.id.imgLogo);
     }
 
-    private void cambiarpantalla(){
-        jugabilidad2_retro_correcta_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                retro_incorrecta.setVisibility(View.VISIBLE);
-                retro_correcta.setVisibility(View.GONE);
-            }
-        });
+    private void Validar(){
+        //Intent intent = getIntent();
+        //int validar = intent.getIntExtra("validarrespuesta", 0);
+        //String retro =  intent.getStringExtra("retroalimentacion");
+        int validar = 0;
 
-        jugabilidad2_retro_incorrecta_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                retro_incorrecta.setVisibility(View.GONE);
-                retro_correcta.setVisibility(View.VISIBLE);
-            }
-        });
+        if(validar == 1){
+            r_felicidades_correcta.setText("Felicidades");
+            imgcheck.setImageResource(R.drawable.aceptar);
+            imglogo.setImageResource(R.drawable.luna_mascota);
+        }else if(validar == 0){
+            r_felicidades_correcta.setText("Respuesta incorrecta");
+            imgcheck.setImageResource(R.drawable.cancelar);
+            imglogo.setImageResource(R.drawable.respuesta_incorrecta);
+        }
     }
 }
